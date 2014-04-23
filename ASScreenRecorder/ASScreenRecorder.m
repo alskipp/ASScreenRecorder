7ea39270ce73f722505665df6a7587cfd6b81cd8
al_skipp <al_skipp@fastmail.fm>
2014-04-23 18:59:06 +0100

    Call drawViewHierarchyInRect: on main queue
    Prevents the view hierarchy mutating whilst drawing into context.

--- ASScreenRecorder/ASScreenRecorder.m
+++ ASScreenRecorder/ASScreenRecorder.m
@@ -234,16 +234,18 @@
         CVPixelBufferRef pixelBuffer = NULL;
         CGContextRef bitmapContext = [self createPixelBufferAndBitmapContext:&pixelBuffer];
         
-        UIGraphicsPushContext(bitmapContext); {
-            if (self.delegate) {
-                [self.delegate writeBackgroundFrameInContext:&bitmapContext];
-            }
-            // draw each window into the context (other windows include UIKeyboard, UIAlert)
-            // FIX: UIKeyboard is currently only rendered correctly in portrait orientation
-            for (UIWindow *window in [[UIApplication sharedApplication] windows]) {
-                [window drawViewHierarchyInRect:CGRectMake(0, 0, _viewSize.width, _viewSize.height) afterScreenUpdates:NO];
-            }
-        } UIGraphicsPopContext();
+        if (self.delegate) {
+            [self.delegate writeBackgroundFrameInContext:&bitmapContext];
+        }
+        // draw each window into the context (other windows include UIKeyboard, UIAlert)
+        // FIX: UIKeyboard is currently only rendered correctly in portrait orientation
+        dispatch_sync(dispatch_get_main_queue(), ^{
+            UIGraphicsPushContext(bitmapContext); {
+                for (UIWindow *window in [[UIApplication sharedApplication] windows]) {
+                                        [window drawViewHierarchyInRect:CGRectMake(0, 0, _viewSize.width, _viewSize.height) afterScreenUpdates:NO];
+                }
+            } UIGraphicsPopContext();
+        });
         
         // append pixelBuffer on a async dispatch_queue, the next frame is rendered whilst this one appends
         // must not overwhelm the queue with pixelBuffers, therefore:
