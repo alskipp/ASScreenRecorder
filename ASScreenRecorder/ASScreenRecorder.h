c5b50e79d44193d7b03b0f09beb7d0e9e4ccc61c
al_skipp <al_skipp@fastmail.fm>
2014-04-23 17:31:42 +0100

    Added comments

--- ASScreenRecorder/ASScreenRecorder.h
+++ ASScreenRecorder/ASScreenRecorder.h
@@ -12,6 +12,8 @@ typedef void (^VideoCompletionBlock)(void);
 
 @interface ASScreenRecorder : NSObject
 @property (nonatomic, readonly) BOOL isRecording;
+
+// delegate is only required when implementing ASScreenRecorderDelegate - see below
 @property (nonatomic, weak) id <ASScreenRecorderDelegate> delegate;
 
 // if saveURL is nil, video will be saved into camera roll
@@ -23,6 +25,13 @@ typedef void (^VideoCompletionBlock)(void);
 @end
 
 
+// If your view contains an AVCaptureVideoPreviewLayer or an openGL view
+// you'll need to write that data into the CGContextRef yourself.
+// In the viewcontroller responsible for the AVCaptureVideoPreviewLayer / openGL view
+// set yourself as the delegate for ASScreenRecorder.
+// [ASScreenRecorder sharedInstance].delegate = self
+// Then implement 'writeBackgroundFrameInContext:(CGContextRef*)contextRef'
+// use 'CGContextDrawImage' to draw your view into the provided CGContextRef
 @protocol ASScreenRecorderDelegate <NSObject>
 - (void)writeBackgroundFrameInContext:(CGContextRef*)contextRef;
 @end
