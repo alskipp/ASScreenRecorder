c9a4303dbe2171e96080f72912bce423ccf7dd52
al_skipp <al_skipp@fastmail.fm>
2014-04-23 15:51:35 +0100

    Removed comment

--- ExampleRecorder/ExampleRecorder/MasterViewController.m
+++ ExampleRecorder/ExampleRecorder/MasterViewController.m
@@ -87,7 +87,6 @@
 
 - (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath
 {
-    // Return NO if you do not want the specified item to be editable.
     return YES;
 }
 
