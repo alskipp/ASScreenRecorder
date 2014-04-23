8db9b7facfae213cdfa20d529ae941c0235890c4
al_skipp <al_skipp@fastmail.fm>
2014-04-23 15:05:10 +0100

    Initial commit

---
+++ ExampleRecorder/ExampleRecorder/DetailViewController.h
@@ -0,0 +1,16 @@
+//
+//  DetailViewController.h
+//  ExampleRecorder
+//
+//  Created by Alan Skipp on 23/04/2014.
+//  Copyright (c) 2014 Alan Skipp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailViewController : UIViewController
+
+@property (strong, nonatomic) id detailItem;
+
+@property (weak, nonatomic) IBOutlet UILabel *detailDescriptionLabel;
+@end
---
+++ ExampleRecorder/ExampleRecorder/MasterViewController.h
@@ -0,0 +1,13 @@
+//
+//  MasterViewController.h
+//  ExampleRecorder
+//
+//  Created by Alan Skipp on 23/04/2014.
+//  Copyright (c) 2014 Alan Skipp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MasterViewController : UITableViewController
+
+@end
